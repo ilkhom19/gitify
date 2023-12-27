30fbc625bfa92acf75b62d7077c70c9225189de4
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2023-12-27 16:55:16 +0600

    demo modify

--- src/GitifyDemo.java
+++ src/GitifyDemo.java
@@ -20,8 +20,19 @@ public class GitifyDemo {
 
         // New Commit
         gitLibrary.createCommit(tree, "John Doe", "Init Commit");
+        gitLibrary.createCommit(tree, "John Doe", "Second Commit"); // Duplicate Commit, will be ignored
+
+        Blob blob3 = new Blob("File Content 3");
+        Tree tree2 = new Tree();
+        tree2.addEntry("file3.txt", blob3);
+        tree2.addEntry("tree", tree);
+        gitLibrary.createCommit(tree2, "John Doe", "Third Commit");
 
         // List Commits
+        System.out.println("++++++++++++++++++++++++++++++");
+        System.out.println("List Commits");
+        System.out.println("++++++++++++++++++++++++++++++\n");
+
         List<Commit> commits = gitLibrary.listCommits();
         for (Commit commit : commits) {
             commit.printContent();
@@ -29,10 +40,43 @@ public class GitifyDemo {
         }
 
         // Search Commit by Hash
+        System.out.println("\n++++++++++++++++++++++++++++++");
+        System.out.println("Search Commit by Hash: " + commits.get(0).getHash());
+        System.out.println("++++++++++++++++++++++++++++++\n");
+
         Commit foundCommit = gitLibrary.findCommitByHash(commits.get(0).getHash());
         if (foundCommit != null) {
-            System.out.println("Found Commit:");
             foundCommit.printContent();
+        } else {
+            System.out.println("Commit not found");
+        }
+
+        // Search Commit by Author
+        System.out.println("\n++++++++++++++++++++++++++++++");
+        System.out.println("Search Commit by Author: John Doe");
+        System.out.println("++++++++++++++++++++++++++++++\n");
+
+        List<Commit> foundCommits = gitLibrary.findCommitsByAuthor("John Doe");
+        if (foundCommits != null) {
+            for (Commit commit : foundCommits) {
+                commit.printContent();
+                System.out.println("--------------");
+            }
+        } else {
+            System.out.println("Commit not found");
+        }
+
+        // Search Commit by Tree
+        System.out.println("\n++++++++++++++++++++++++++++++");
+        System.out.println("Search Commit by Tree: " + tree.getHash());
+        System.out.println("++++++++++++++++++++++++++++++\n");
+
+        foundCommits = gitLibrary.findCommitsByTree(tree.getHash());
+        if (foundCommits != null) {
+            for (Commit commit : foundCommits) {
+                commit.printContent();
+                System.out.println("--------------");
+            }
         } else {
             System.out.println("Commit not found");
         }
