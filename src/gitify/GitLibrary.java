b85f332b58fed8a357490b6725b02c1c7f4c5ff4
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2023-12-27 16:22:13 +0600

    Search in HasMap

--- src/gitify/GitLibrary.java
+++ src/gitify/GitLibrary.java
@@ -1,31 +1,27 @@
 package gitify;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class GitLibrary {
-    private final List<Commit> commits;
+    private List<Commit> commits;
+    private Map<String, Commit> commitHashMap;
 
     public GitLibrary() {
         this.commits = new ArrayList<>();
+        this.commitHashMap = new HashMap<>();
     }
 
     public void createCommit(Tree tree, String author, String message) {
         Commit commit = new Commit(tree, author, message);
         commits.add(commit);
+        commitHashMap.put(commit.getHash(), commit);
     }
 
     public List<Commit> listCommits() {
         return Collections.unmodifiableList(commits);
     }
 
-    public Commit findCommitByHash(String hash) {
-        for (Commit commit : commits) {
-            if (commit.getHash().equals(hash)) {
-                return commit;
-            }
-        }
-        return null;
+    public Commit findCommitByHash(String hash){
+        return commitHashMap.getOrDefault(hash, null);
     }
 }
